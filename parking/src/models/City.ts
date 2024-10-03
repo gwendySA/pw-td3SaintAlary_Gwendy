485d541d4ff2528f4413c7a4698691e18a6ae34f
gwendySA <saintalaryg@gmail.com>
2024-10-03 14:39:07 +0200

    nouvelle version

--- parking/src/models/City.ts
+++ parking/src/models/City.ts
@@ -18,40 +18,5 @@ export class City {
         this._country = country;
         this._location = { latitude, longitude };
     }
-    get id(): number {
-        return this._id;
-    }
-    set id(value: number) {
-        this._id = value;
-    }
-    get name(): string {
-        return this._name;
-    }
-    set name(value: string) {
-        this._name = value;
-    }
-    get slug(): string {
-        return this._slug;
-    }
-    set slug(value: string) {
-        this._slug = value;
-    }
-    get parkingIds(): Array<number> {
-        return this._parkingIds;
-    }
-    set parkingIds(value: Array<number>) {
-        this._parkingIds = value;
-    }
-    get country(): string {
-        return this._country;
-    }
-    set country(value: string) {
-        this._country = value;
-    }
-    get location(): GPS {
-        return this._location;
-    }
-    set location(value: GPS) {
-        this._location = value;
-    }
+
 }
