84dce5cee55a685f0fbd058c90d05a632029ab89
gwendySA <saintalaryg@gmail.com>
2024-10-03 14:47:56 +0200

    nouvelle version

--- parking/src/models/City.ts
+++ parking/src/models/City.ts
@@ -3,20 +3,20 @@ import {generateRandomNumberId} from "../utils/generateRandomNumberld";
 import { GPS } from "../types/GPS";
 
 export class City {
-    private _id: number;
-    private _name: string;
-    private _slug: string;
-    private _parkingIds: Array<number>;
-    private _country: string;
-    private _location: GPS;
+    id: number;
+    name: string;
+    slug: string;
+    parkingIds: Array<number>;
+    country: string;
+    location: GPS;
 
     constructor(name: string, country: string, latitude: number, longitude: number) {
-        this._id = generateRandomNumberId();
-        this._name = name;
-        this._slug = toSlug(name);
-        this._parkingIds = [];
-        this._country = country;
-        this._location = { latitude, longitude };
+        this.id = generateRandomNumberId();
+        this.name = name;
+        this.slug = toSlug(name);
+        this.parkingIds = [];
+        this.country = country;
+        this.location = { latitude, longitude };
     }
 
 }
