76669006b4a74d10ad35e77e81ab8a846919bba6
gwendySA <saintalaryg@gmail.com>
2024-11-16 17:27:53 +0100

    Update toSlug.test.ts

--- parking/tests/toSlug.test.ts
+++ parking/tests/toSlug.test.ts
@@ -2,7 +2,7 @@ import { expect, describe,it } from "bun:test";
 import {toSlug} from "../src/utils/toSlug";
 
 describe('toSlug', () => {
-    it('should convert a string to a slug', () => {
+    it('devrait faire des slugs', () => {
         expect(toSlug('Je suis 1 cas d\'école')).toBe('je-suis-1-cas-d-ecole');
         expect(toSlug('Éléphant à l\'école')).toBe('elephant-a-l-ecole');
         expect(toSlug('  Espaces  en  trop  ')).toBe('espaces-en-trop');
