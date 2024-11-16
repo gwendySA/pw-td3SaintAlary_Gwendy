bfe9c79e8e39d6feb0f35636bf806efe516ecda3
gwendySA <saintalaryg@gmail.com>
2024-11-16 17:25:11 +0100

    Update bun-types.d.ts

--- parking/src/types/bun-types.d.ts
+++ parking/src/types/bun-types.d.ts
@@ -1,4 +1,4 @@
-/// généré par l'ia a partir d'un forum car il y avait un soucis qui nous bloquaient />
+/// généré par l'ia a partir d'un forum car il y avait un soucis qui nous bloquaient avec sqlite ( ne voulait pas se lancer sur 2 des 3 PC) />
 declare module "bun:sqlite" {
     export class Database {
         constructor(filename: string);
